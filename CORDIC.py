99088eabbb042639ee1457166b258e9174f71dd4
sarobinson2011 <58254703+sarobinson2011@users.noreply.github.com>
2022-12-22 08:43:12 +0000

    Create CORDIC.py
    Python implementation of the CORDIC algorithm. Function takes an angle in radians and the number of iterations to perform as input, and returns the sine and cosine of the angle as output.

---
+++ CORDIC.py
@@ -0,0 +1,32 @@
+import math
+
+""" takes an angle in radians and the number of iterations to perform as input, 
+   and returns the sine and cosine of the angle as output """
+
+def cordic_sine_cosine(angle, num_iterations):
+    # Initialize variables
+    x = 1.0
+    y = 0.0
+    z = angle
+    for i in range(num_iterations):
+        # Calculate next iteration
+        d = 1.0 if z > 0 else -1.0
+        x_new = x - d * y / (2**i)
+        y_new = y + d * x / (2**i)
+        z_new = z - d * math.atan(1 / (2**i))
+        # Update variables
+        x = x_new
+        y = y_new
+        z = z_new
+    # Calculate sine and cosine
+    sine = y
+    cosine = x
+    return sine, cosine
+
+""" The number of iterations determines the accuracy of the result; 
+   more iterations will result in a more accurate answer, but will also take longer to compute. """
+
+angle = math.pi / 4  # 45 degrees in radians
+num_iterations = 10
+sine, cosine = cordic_sine_cosine(angle, num_iterations)
+print(f'sine({angle}): {sine}, cosine({angle}): {cosine}')
